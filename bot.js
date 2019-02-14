f727d1e6d70c153fcf83413c6190bf18ae0e7fd8
ReTired7 <47307366+ReTired7@users.noreply.github.com>
2019-02-14 18:55:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,6 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : Retir'ed ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`ليش الفرح بيجلس فترح ويرحل والحزن يفضل معك للغد
+client.user.setGame(`ليش الفرح يجلس فتره ويرحل والحزن يجلس معك للغد `,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
