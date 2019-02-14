1ee9fc5df4ecc62950922825705e5a31f847e105
ReTired7 <47307366+ReTired7@users.noreply.github.com>
2019-02-14 18:54:19 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = "=";
+
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : Retir'ed`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : Retir'ed ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`ليش الفرح بيجلس فترح ويرحل والحزن يفضل معك للغد
+client.user.setStatus("dnd")
+});
